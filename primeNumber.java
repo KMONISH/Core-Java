0ec81e3d94fe5c6788d05075d7bb4b4be93fb157
KMONISH <monishkb1999@gmail.com>
2025-07-18 21:15:02 +0530

    'one'

---
+++ primeNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class primeNumber {
+   public static void main(String[] args){
+
+       int inputNum;
+       int count= 0;
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Enter the number");
+       inputNum = scanner.nextInt();
+
+       for(int i=1;i<=inputNum;i++) {
+           if (inputNum % i == 0) {
+               count = count + 1;
+           }
+       }
+       if (count == 2) {
+           System.out.println("Its a Prime number");
+       } else
+           System.out.println("Its not a Prime number");
+   }
+}
+
+//output
+//11
+//its a prime number
+
+//8
+//not a prime number
