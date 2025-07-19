642174698ba18f63e0b102451bf6750691a1c5d1
KMONISH <monishkb1999@gmail.com>
2025-07-19 20:02:27 +0530

    corejava

---
+++ factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class factorial {
+        public static void main(String[] args){
+            int fact=1;
+            Scanner scanner =new Scanner(System.in);
+            System.out.println("Enter the number to calculate Palindrome -");
+            int num = scanner.nextInt();
+            for(int i=1;i<=num;i++){
+                 fact= fact *i;
+            }
+            System.out.println("Factorial is: "+fact);
+        }
+    }
+
+//Enter the number to calculate Palindrome -
+//        5
+//Factorial is: 120
